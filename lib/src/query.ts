4811897e878943a15924ea0c92ef3a7caa8804df
Chris Fraser <chris@otterdev.io>
2023-10-11 09:57:45 +1100

    more module tweaking

--- lib/src/query.ts
+++ lib/src/query.ts
@@ -1,5 +1,3 @@
-import groq from "groq";
-
 /**
  * Use in your groq queries to ensure images have all the necessary metadata to be useful in image tags
  *
@@ -20,7 +18,7 @@ import groq from "groq";
  * @param opts.as what the attribute should be named in our output object
  * @returns enhanced groq fragment
  */
-export const image = (attr: string, opts?: { as: string }) => groq.default`'${
+export const image = (attr: string, opts?: { as: string }) => `'${
   opts?.as ?? attr
 }': ${attr} {
   ...,
