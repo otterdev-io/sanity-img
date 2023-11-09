6e759f7b829e208516eaf1de4da9852b9902e8cd
Chris Fraser <chris@otterdev.io>
2023-11-09 17:09:27 +1100

    Inject image builder setting into astro client side pre-hydration

--- astro/src/integration.ts
+++ astro/src/integration.ts
@@ -8,13 +8,7 @@ export default function sanityImg(
   defaults?: SanityImgDefaults,
 ): AstroIntegration {
   const resolvedDefaults = defaults ?? defaultSanityImgDefaults;
-  return {
-    name: "@otterstack/sanity-img-astro/integration",
-    hooks: {
-      "astro:config:setup": ({ injectScript }) => {
-        injectScript(
-          "page-ssr",
-          `
+  const loadImageBuilder = `
           import $_sic_urlBuilder from "@sanity/image-url";
           import { sanityClient as $_sic_sanityClient } from "sanity:client";
           import { setSanityImgDefaults as $_sic_setSanityImgDefaults } from "@otterstack/sanity-img-astro";
@@ -24,8 +18,15 @@ export default function sanityImg(
               resolvedDefaults,
             )}, imageUrlBuilder });
           }
-        `,
-        );
+        `;
+  return {
+    name: "@otterstack/sanity-img-astro/integration",
+    hooks: {
+      "astro:config:setup": ({ injectScript }) => {
+        //For ssr rendering
+        injectScript("page-ssr", loadImageBuilder);
+        //For client side components (svelte)
+        injectScript("before-hydration", loadImageBuilder);
       },
     },
   };
